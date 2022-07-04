5954053277de2ecc737ce65fe56a51f3cc15d631
F6239902 Leticia Ferreira Reis <leticia.reis@bb.com.br>
2022-07-04 17:19:30 -0300

    Criando o cliente

---
+++ src/main/java/br/shop/bb/model/Cards.java
@@ -0,0 +1,27 @@
+package br.shop.bb.model;
+
+public class Cards {
+    private String number;
+    private String code; 
+    private String flag;
+    
+    public String getNumber() {
+        return number;
+    }
+    public void setNumber(String number) {
+        this.number = number;
+    }
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+    public String getFlag() {
+        return flag;
+    }
+    public void setFlag(String flag) {
+        this.flag = flag;
+    }
+    
+}
