c427a6ce48969c049e53ecb3d9ab0097071edcc9
Igor <igor-rh@hotmail.com>
2022-07-04 17:59:03 -0300

    Creating BaseRepository

--- src/main/java/br/shop/bb/model/Client.java
+++ src/main/java/br/shop/bb/model/Client.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 
 import br.shop.bb.annotations.ID;
 
-public class Client {
+public class Client extends BaseModel<Integer> {
     @ID
     private int idClient;
     private Person person;
---
+++ src/main/java/br/shop/bb/repository/PurchaseRepository.java
@@ -0,0 +1,49 @@
+package br.shop.bb.repository;
+
+import java.util.List;
+
+import br.shop.bb.model.Purchase;
+
+public class PurchaseRepository implements BaseRepository<Purchase, Integer> {
+
+    @Override
+    public void persistir(Purchase entity) {
+        // TODO Auto-generated method stub
+        
+        
+    }
+
+    @Override
+    public Integer criarId() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void atualizar(Purchase entity) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Purchase getById(Integer id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Purchase> listarTodos() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void excluir(Purchase entity) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+    
+    
+}
