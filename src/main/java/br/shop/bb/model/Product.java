86c3d30806ad64e6903b72899bf60274b89956aa
Igor <igor-rh@hotmail.com>
2022-07-04 17:49:37 -0300

    Removing TypeProduct out of Product class

--- src/main/java/br/shop/bb/model/Product.java
+++ src/main/java/br/shop/bb/model/Product.java
@@ -1,17 +1,13 @@
 package br.shop.bb.model;
 
-
+import br.shop.bb.annotations.ID;
+import br.shop.bb.enums.TypeProduct;
 //Product {idProduct, name, description, price, typeProduct(class, enum){......,...., etc}}
-public class Product {
+public class Product extends BaseModel<Integer> {
 
-    public enum TypeProduct {
-        FOOD,
-        DRINK,
-        CLOTHES,
-        ACCESSORIES,
-        OTHER
-    }
-
+    
+    
+    @ID
     private int idProduct;
     private String name;
     private String description;
