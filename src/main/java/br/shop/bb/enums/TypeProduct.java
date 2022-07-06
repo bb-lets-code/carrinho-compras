528c7fdadaef6bc15a71a98eeeae266ded40a676
Maria Luiza Ferreira Assumpção Almeida <mlfaa@hotmail.com>
2022-07-06 14:00:44 -0300

    Adding functions in type product enum

--- src/main/java/br/shop/bb/enums/TypeProduct.java
+++ src/main/java/br/shop/bb/enums/TypeProduct.java
@@ -1,9 +1,26 @@
 package br.shop.bb.enums;
 
+import java.math.BigDecimal;
+
 public enum TypeProduct {
-    FOOD,
-    DRINK,
-    CLOTHES,
-    ACCESSORIES,
-    OTHER
+    FOOD(0.1, 0.1),
+    DRINK(0.2, 0.0),
+    CLOTHES(0.3, 0.6),
+    ACCESSORIES(0.4, 0.5),
+    OTHER(0.5, 0.0);
+
+    double discount;
+    double fee;
+
+    TypeProduct(double discount, double fee){
+        this.discount = discount;
+        this.fee = fee;
+    }
+    public double getDiscount() {
+        return discount;
+    }
+
+    public double getFee() {
+        return fee;
+    }
 }
