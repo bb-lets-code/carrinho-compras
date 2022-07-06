625aea82a7e062e0c89cfa1f838045a7d142a61e
Maria Luiza Ferreira Assumpção Almeida <mlfaa@hotmail.com>
2022-07-06 16:57:51 -0300

    Refactoring addProducts method

--- src/main/java/br/shop/bb/services/cart/AddProduct.java
+++ src/main/java/br/shop/bb/services/cart/AddProduct.java
@@ -3,12 +3,13 @@ package br.shop.bb.services.cart;
 import br.shop.bb.model.Cart;
 import br.shop.bb.model.Product;
 
-public class AddCart {
-    public void addProducts(Cart cart, Product product) {
-        if(cart.getProducts().containsValue(product)){
+public class AddProduct {
+    public static void addProducts(Cart cart, Product product) {
+        if (cart.getProducts().containsKey(product)) {
             cart.getProducts().replace(product, cart.getProducts().get(product).intValue() + 1);
         } else {
             cart.getProducts().put(product, 1);
         }
+        CalculateTotal.calculate(cart, product);
     }
 }
