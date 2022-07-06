0a1023f90f25a617a358c6bed07440e1c923d1d1
Maria Luiza Ferreira Assumpção Almeida <mlfaa@hotmail.com>
2022-07-06 17:00:02 -0300

    Creating test cart

--- src/main/java/br/shop/bb/main/App.java
+++ src/main/java/br/shop/bb/main/App.java
@@ -1,12 +1,26 @@
 package br.shop.bb.main;
 
 import br.shop.bb.enums.TypeProduct;
+import br.shop.bb.model.Cart;
 import br.shop.bb.model.Client;
 import br.shop.bb.model.Product;
+import br.shop.bb.services.cart.AddProduct;
+
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) {
-        Product product = new Product("Abacaxi", "Fruta", 3.0, TypeProduct.FOOD);
+        Product product = new Product("Abacaxi", "Fruta", 10.0, TypeProduct.FOOD);
         Client client = new Client();
+
+        Cart cart = new Cart();
+        AddProduct.addProducts(cart, product);
+        AddProduct.addProducts(cart, product);
+
+        System.out.println(cart.getTotal());
+
+        for (Map.Entry<Product,Integer> value : cart.getProducts().entrySet()) {
+            System.out.println(value.getKey().getName() + " - " + value.getValue());
+        }
     }
 }
