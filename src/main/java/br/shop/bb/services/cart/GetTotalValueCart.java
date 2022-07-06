258ae230bc55a0f38be1783e0dde26ca12e53daf
Maria Luiza Ferreira Assumpção Almeida <mlfaa@hotmail.com>
2022-07-06 14:01:55 -0300

    creating function to get total value in cart

---
+++ src/main/java/br/shop/bb/services/cart/GetTotalValueCart.java
@@ -0,0 +1,12 @@
+package br.shop.bb.services.cart;
+
+import br.shop.bb.model.Cart;
+import br.shop.bb.model.Product;
+
+import java.util.HashMap;
+
+public class GetTotalValueCart {
+    public void getTotalValueCart(Cart cart) {
+        //CHAI OF RESPONSABILITY
+    }
+}
