4cfe995a969a7cc81d0fccd2581afb021bd31793
Igor <igor-rh@hotmail.com>
2022-07-07 19:15:56 -0300

    Factory para iniciar programa

--- src/main/java/br/shop/bb/main/App.java
+++ src/main/java/br/shop/bb/main/App.java
@@ -5,26 +5,38 @@ import br.shop.bb.model.Cart;
 import br.shop.bb.model.Product;
 import br.shop.bb.services.cart.AddProduct;
 import br.shop.bb.services.cart.RemoveProduct;
+import br.shop.bb.services.cart.UpdateQtProductCart;
 import br.shop.bb.services.cart.ViewCart;
 
 public class App {
     public static void main(String[] args) {
-        Product product = new Product("Abacaxi", "Fruta", 10.0, TypeProduct.FOOD);
-        Product product1 = new Product("Blusinha", "Fashion", 50.0, TypeProduct.CLOTHES);
+        Product abacaxi = new Product("Abacaxi", "Fruta", 10.0, TypeProduct.FOOD);
+        Product blusinha = new Product("Blusinha", "Fashion", 50.0, TypeProduct.CLOTHES);
         Cart cart = new Cart();
         AddProduct addProduct = new AddProduct();
         ViewCart viewCart = new ViewCart();
 
         // addProduct.addProducts(cart, product);
-        addProduct.addProducts(cart, product);
-        addProduct.addProducts(cart, product1);
+        System.out.println("Add");
+        addProduct.addProducts(cart, abacaxi);
+        addProduct.addProducts(cart, blusinha);
 
+            
         viewCart.printCart(cart);
 
         RemoveProduct removeProduct = new RemoveProduct();
+        System.out.println("Remove");
         
-        removeProduct.removeProductCart(cart, product1);
+        removeProduct.removeProductCart(cart, blusinha);
+        UpdateQtProductCart updateQtProductCart = new UpdateQtProductCart();
         viewCart.printCart(cart);
 
+        System.out.println(" ------------- Update");
+
+        updateQtProductCart.updateProductCart(cart, abacaxi, 5);
+        viewCart.printCart(cart);
+        
+
+        
     }
 }
--- src/main/java/br/shop/bb/services/cart/RemoveProduct.java
+++ src/main/java/br/shop/bb/services/cart/RemoveProduct.java
@@ -1,5 +1,7 @@
 package br.shop.bb.services.cart;
 
+import java.util.Map;
+
 import br.shop.bb.model.Cart;
 import br.shop.bb.model.Product;
 import br.shop.bb.services.cart.calculate.recalculateEvent.listeners.RecalculateListener;
@@ -17,7 +19,19 @@ public class RemoveProduct {
 
     public void removeProductCart(Cart cart, Product product){
         if(cart.getProducts().containsKey(product)){
-            cart.getProducts().remove(product);
+            for (Map.Entry<Product, Integer> value : cart.getProducts().entrySet()) {
+                if(value.getKey().equals(product)){
+                    if(value.getValue() ==1 ) {
+                        cart.getProducts().remove(product);
+                        
+                    }
+                    else {
+                        cart.getProducts().replace(product, value.getValue() - 1);
+                    }
+                break;
+                }
+            }
+            cart.getProducts().get(product); 
             product.setPrice(product.getPrice() * -1.0);
             calculateEventManager.notify("calculateCartTotal",cart, product);
         } else {
