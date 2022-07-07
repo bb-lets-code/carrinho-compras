d7c9e92a0fdbdc7875c2594bf250b8c88f3e1991
F6239902 Leticia Ferreira Reis <leticia.reis@bb.com.br>
2022-07-07 17:31:52 -0300

    Mudanças que o vscode não salvou

--- src/main/java/br/shop/bb/services/validators/Verify.java
+++ src/main/java/br/shop/bb/services/validators/Verify.java
@@ -1,8 +1,8 @@
 package br.shop.bb.services.validators;
 
-import br.shop.bb.model.Person;
+import br.shop.bb.model.Client;
 
 public interface Verify {
-    void verify(Person person);
+    void verify(Client client);
     
 }
