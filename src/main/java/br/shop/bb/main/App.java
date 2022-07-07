dddef20b03890eab70928a5a76df8cf082539896
Igor <igor-rh@hotmail.com>
2022-07-07 17:58:55 -0300

    Main

--- src/main/java/br/shop/bb/main/App.java
+++ src/main/java/br/shop/bb/main/App.java
@@ -24,6 +24,7 @@ public class App {
         RemoveProduct removeProduct = new RemoveProduct();
         
         removeProduct.removeProductCart(cart, product1);
+        viewCart.printCart(cart);
 
     }
 }
