bd9dcdec2b607fd2273fa41bfea47cfb73305e80
Igor Rhamon <igor.rh@gmail.com>
2022-07-06 23:53:57 -0300

    Using TotalCart class to recalculate on RecalculateListener

--- src/main/java/br/shop/bb/services/cart/calculate/recalculateEvent/listeners/RecalculateListener.java
+++ src/main/java/br/shop/bb/services/cart/calculate/recalculateEvent/listeners/RecalculateListener.java
@@ -3,13 +3,16 @@ package br.shop.bb.services.cart.calculate.recalculateEvent.listeners;
 import br.shop.bb.interfaces.EventListener;
 import br.shop.bb.model.Cart;
 import br.shop.bb.model.Product;
+import br.shop.bb.services.cart.calculate.TotalCart;
 
 public class RecalculateListener implements EventListener {
     
 
     @Override
     public void recalculate(String eventType, Cart cart, Product product) {
-        // TODO Auto-generated method stub
+        
+        TotalCart calculateTotal = new TotalCart();
+        calculateTotal.calculate(cart, product);
         
     }
 
