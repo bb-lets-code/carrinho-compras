7061f090155255a4000caa5135cb785329d69666
GesielFreitas <gesiel.was.f@gmail.com>
2022-07-07 17:34:19 -0300

    testando na main

--- src/main/java/br/shop/bb/main/App.java
+++ src/main/java/br/shop/bb/main/App.java
@@ -1,11 +1,14 @@
 package br.shop.bb.main;
 
+import br.shop.bb.enums.Installments;
+import br.shop.bb.enums.PaymentType;
 import br.shop.bb.enums.TypeProduct;
-import br.shop.bb.model.Cart;
-import br.shop.bb.model.Client;
-import br.shop.bb.model.Product;
+import br.shop.bb.model.*;
 import br.shop.bb.services.cart.AddProduct;
 import br.shop.bb.services.cart.RemoveProduct;
+import br.shop.bb.services.order.OrderTemplate;
+import br.shop.bb.services.order.OrderBuilder;
+import br.shop.bb.services.order.PayOrder;
 
 import java.util.Map;
 
@@ -39,5 +42,31 @@ public class App {
         for (Map.Entry<Product, Integer> value : cart.getProducts().entrySet()) {
             System.out.println(value.getKey().getName() + " - " + value.getValue());
         }
+
+        // Create Order in Close cart
+
+        Card card = new Card();
+        card.setNumber("4526412365478965");
+        card.setFlag("Visa");
+        card.setCode("452");
+
+        PaymentMethod method = new PaymentMethod();
+        method.setPaymentType(PaymentType.CREDITCARD);
+        Payment payment = new CreditCard(card, Installments.TEN);
+        System.out.println(payment.getPaid());
+        OrderTemplate orderTem = new OrderTemplate();
+        OrderBuilder builder = new OrderBuilder();
+
+        orderTem.constructOrder(cart, method, payment, builder);
+
+        Order order = builder.getOrderResult();
+
+        PayOrder pay = new PayOrder();
+
+        pay.payOrder(order);
+
+        System.out.println(order.getPayment().getPaid());
+        System.out.println(order.getPayment().getTotal());
+
     }
 }
