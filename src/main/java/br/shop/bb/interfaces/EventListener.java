b95eb612dde1746a2059e38c45e57cf580b902aa
Igor Rhamon <igor.rh@gmail.com>
2022-07-06 23:16:31 -0300

    Including name o Event

--- src/main/java/br/shop/bb/interfaces/EventListener.java
+++ src/main/java/br/shop/bb/interfaces/EventListener.java
@@ -4,6 +4,6 @@ import br.shop.bb.model.Cart;
 import br.shop.bb.model.Product;
 
 public interface EventListener {
-     void update( Cart cart, Product product) ;
+     void update(String eventType, Cart cart, Product product) ;
 
 }
