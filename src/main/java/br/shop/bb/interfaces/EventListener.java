fddee2a617a87c0651f5bc9bd5c8c33fac42229c
Igor Rhamon <igor.rh@gmail.com>
2022-07-06 23:36:11 -0300

    Changing name method of interface

--- src/main/java/br/shop/bb/interfaces/EventListener.java
+++ src/main/java/br/shop/bb/interfaces/EventListener.java
@@ -4,6 +4,6 @@ import br.shop.bb.model.Cart;
 import br.shop.bb.model.Product;
 
 public interface EventListener {
-     void update(String eventType, Cart cart, Product product) ;
+     void recalculate(String eventType, Cart cart, Product product) ;
 
 }
