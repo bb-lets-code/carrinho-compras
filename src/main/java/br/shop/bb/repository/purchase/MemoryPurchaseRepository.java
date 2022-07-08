6252ddfc9c24dd50490454b0f069103cd5e1a082
Igor Rhamon <igor.rh@gmail.com>
2022-07-08 08:55:14 -0300

    Purchase Repository

---
+++ src/main/java/br/shop/bb/repository/purchase/MemoryPurchaseRepository.java
@@ -0,0 +1,56 @@
+package br.shop.bb.repository.purchase;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import br.shop.bb.model.Client;
+import br.shop.bb.model.Purchase;
+import br.shop.bb.repository.BaseRepository;
+
+public class MemoryPurchaseRepository implements BaseRepository<Purchase, Integer> {
+    List<Purchase> purchaseList = new ArrayList<Purchase>();
+
+    @Override
+    public void persist(Purchase entity) {
+        int id= entity.getId();
+        if(id == 0) {
+            id = purchaseList.size() + 1;
+            entity.setId(id);
+        }
+        purchaseList.add(entity);
+    }
+
+    @Override
+    public Integer createID() {
+        return purchaseList.size();
+    }
+
+    @Override
+    public void update(Purchase entity) {
+        if(purchaseList.contains(entity)){
+            purchaseList.set(purchaseList.indexOf(entity), entity);
+        }        
+    }
+
+    @Override
+    public Purchase getById(Integer id) {
+        return purchaseList.get(id);
+    }
+
+    @Override
+    public Set<Purchase> findAll() {
+       return null;
+    }
+
+    @Override
+    public void delete(Purchase entity) {
+        if(purchaseList.contains(entity)){
+            purchaseList.remove(entity);
+        }
+    }
+
+
+    
+    
+}
