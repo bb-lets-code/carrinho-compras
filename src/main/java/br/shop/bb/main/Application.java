5af71112294935857e0bdb23c886fac1f77e1d6d
Igor <igor-rh@hotmail.com>
2022-07-08 17:16:25 -0300

    Removendo comentários

--- src/main/java/br/shop/bb/main/Application.java
+++ src/main/java/br/shop/bb/main/Application.java
@@ -26,20 +26,7 @@ import br.shop.bb.services.order.OrderBuilder;
 import br.shop.bb.services.order.OrderTemplate;
 import br.shop.bb.services.order.PayOrder;
 
-/**
- * O cliente pode ser PessoaFisica ou PessoaJuridica
-Criar um método para listar os itens no carrinho (Produto, Quantidade, Preço Unitário e Total por item)
-Criar promoção para tipo de produto. Prever o desconto no momento de calcular o total da compra.
-Calcular o total a pagar do carrinho de compras.
-Criar um método para adicionar um item no carrinho.
-Criar um método para remover um item do carrinho.
-Criar um método para alterar a quantidade de um item do carrinho.
-Ao adicionar um novo produto verificar se o produto já existe, se existir atualizar apenas a quantidade.
-Ao iniciar o programa verificar de onde recuperar os dados dos produtos, ter a opção de buscar os itens fixos no database ou de uma outra fonte de dados (esta outra fonte de dados não precisa ser implementada, apenas simulada).
-Apenas para alguns produtos prever a cobrança de uma taxa extra a ser adicionado no valor total da compra.
-Apenas para alguns produtos prever a cobrança de frete (pode ser valor fixo ou porcentagem).
-Criar classes de repository para manipulação e armazenamento dos dados (CRUD).
- */
+
 public class Application {
     public static void main(String[] args) {
         
