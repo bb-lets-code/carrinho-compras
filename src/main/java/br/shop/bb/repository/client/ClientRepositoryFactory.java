f3c61072a017837e42d5e49d32912c990f13f80e
Igor Rhamon <igor.rh@gmail.com>
2022-07-08 08:54:26 -0300

    Client Repository Factory

---
+++ src/main/java/br/shop/bb/repository/client/ClientRepositoryFactory.java
@@ -0,0 +1,14 @@
+package br.shop.bb.repository.client;
+
+
+import br.shop.bb.model.Client;
+import br.shop.bb.repository.BaseRepository;
+
+public class ClientRepositoryFactory {
+    public BaseRepository<Client, Integer> getBaseRepository(String type) {
+        if (type.equals("MemoryClient")) {
+            return new MemoryClientRepository();
+        }
+        return null;
+    }
+}
